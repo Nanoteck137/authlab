cbd3809285d7ae9baa6cfa1728fc04b35d71276b
Patrik M. Rosenström <patrik.millvik@gmail.com>
2026-01-06 15:01:31 +0100

    Updated config to have the odic variables

--- config/config.go
+++ config/config.go
@@ -10,10 +10,14 @@ import (
 )
 
 type Config struct {
-	RunMigrations   bool   `mapstructure:"run_migrations"`
-	ListenAddr      string `mapstructure:"listen_addr"`
-	DataDir         string `mapstructure:"data_dir"`
-	JwtSecret       string `mapstructure:"jwt_secret"`
+	RunMigrations    bool   `mapstructure:"run_migrations"`
+	ListenAddr       string `mapstructure:"listen_addr"`
+	DataDir          string `mapstructure:"data_dir"`
+	OdicClientId     string `mapstructure:"odic_client_id"`
+	OdicClientSecret string `mapstructure:"odic_client_secret"`
+	OdicIssuerUrl    string `mapstructure:"odic_issuer_url"`
+	OdicRedirectUrl  string `mapstructure:"odic_redirect_url"`
+	JwtSecret        string `mapstructure:"jwt_secret"`
 }
 
 func (c *Config) WorkDir() types.WorkDir {
@@ -24,6 +28,10 @@ func setDefaults() {
 	viper.SetDefault("run_migrations", "true")
 	viper.SetDefault("listen_addr", ":3000")
 	viper.BindEnv("data_dir")
+	viper.BindEnv("odic_client_id")
+	viper.BindEnv("odic_client_secret")
+	viper.BindEnv("odic_issuer_url")
+	viper.BindEnv("odic_redirect_url")
 	viper.BindEnv("jwt_secret")
 }
 
@@ -41,6 +49,10 @@ func validateConfig(config *Config) {
 	// validate(config.RunMigrations == "", "run_migrations needs to be set")
 	validate(config.ListenAddr == "", "listen_addr needs to be set")
 	validate(config.DataDir == "", "data_dir needs to be set")
+	validate(config.OdicClientId == "", "odic_client_id needs to be set")
+	validate(config.OdicClientSecret == "", "odic_client_secret needs to be set")
+	validate(config.OdicIssuerUrl == "", "odic_issuer_url needs to be set")
+	validate(config.OdicRedirectUrl == "", "odic_redirect_url needs to be set")
 	validate(config.JwtSecret == "", "jwt_secret needs to be set")
 
 	if hasError {
@@ -77,6 +89,8 @@ func InitConfig() {
 	}
 
 	configCopy := LoadedConfig
+	configCopy.OdicClientId = "***"
+	configCopy.OdicClientSecret = "***"
 	configCopy.JwtSecret = "***"
 
 	slog.Info("Current Config", "config", configCopy)
